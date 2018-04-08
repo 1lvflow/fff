e0c0ed37081b72dec50d5591677f2ab5c1cd0542
1tiaogou <304252714@qq.com>
2018-04-08 22:04:49 +0800

    wa

--- src/test/java/ff/AppTest.java
+++ src/test/java/ff/AppTest.java
@@ -136,7 +136,10 @@ public class AppTest
             int end = vi.indexOf(en);
             String vid = vi.substring(begin + bg.length(), end);
             String iframe = iframeBg + vid + iframeEnd;
-            video.attr("src", iframe).attr("width", "100%").attr("height", "auto");
+            video.attr("src", iframe).
+                    attr("width", "100%")
+                    .attr("height", "auto")
+                    .attr("allowfullscreen", "true");
         });
         return doc;
     }
